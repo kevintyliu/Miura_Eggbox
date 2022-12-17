7093bf6c0a9b8543cfa37e0bf8855dd44f928e76
Kevin Liu <kl6599@princeton.edu>
2022-12-17 16:45:53 -0500

    Visualize Tubes

--- initVisual.m
+++ initVisual.m
@@ -2,14 +2,20 @@ function [f] = initVisual(m,n,miura_bools)
 f = figure;
 f.Position = [100 100 1000 800];
 axes('Parent',f,'position',[0.1 0.4  0.5 0.55]);
-hold on;
-for i = 1:4*m*n
+for i = 1:4*sum(~miura_bools)
+    fill3(1,1,1,'g'); % Miura flip
+    hold on;
+end
+for i = 1:4*m*n 
     if miura_bools(end-ceil(i/(4*m))+1)
         fill3(1,1,1,[0 0.4470 0.7410]);
+        fill3(1,1,1,[0 0.4470 0.7410]); % for tubes
     else
         fill3(1,1,1,[0.8500 0.3250 0.0980]);
+        fill3(1,1,1,[0.8500 0.3250 0.0980]);% for tubes
     end
 end
+
 alpha(0.5);
 grid on
 axis equal
--- plotMiuraEgg.m
+++ plotMiuraEgg.m
@@ -21,18 +21,55 @@ Xs = [Xms; Xes];
 Ys = [Yms; Yes];
 Zs = [Zms; Zes];
 quadInds = [mInds; eInds];
-for i = 1:size(quadInds,1)
-    h = f.Children(length(f.Children)).Children(i);
-    h.XData = Xs(quadInds(i,:));
-    h.YData = Ys(quadInds(i,:));
-    h.ZData = Zs(quadInds(i,:));
-end
+% nhats = zeros(3,size(quadInds,1));
+
 axes(f.Children(length(f.Children)));
 title(sprintf('$a$: %0.2f, $b_m$: %0.2f, $b_e$: %0.2f, $\\gamma_m$: %0.1f$^\\circ$, $\\gamma_e$: %0.2f$^\\circ$, $\\psi$: %0.1f$^\\circ$',...
     a,bm,be,rad2deg(gm),rad2deg(ge),rad2deg(psi)),'Interpreter','latex');
 xlim([0,2*bm*sin(gm)+2*be*sin(ge)]);
 ylim([0,max([2*a+bm*cos(gm) bm+2*a*cos(gm) 2*a*sin(ge)])]);
-zlim([0,max([a+be*cos(ge) be+a*cos(ge) a*sin(gm)])]);
+% zlim([0,max([a+be*cos(ge) be+a*cos(ge) a*sin(gm)])]);
+zlim([-max([a+be*cos(ge) be+a*cos(ge) a*sin(gm)]),max([a+be*cos(ge) be+a*cos(ge) a*sin(gm)])]);
+for i = 1:size(quadInds,1)
+    h = f.Children(length(f.Children)).Children(2*i-1); % for tube
+    h.XData = Xs(quadInds(i,:));
+    h.YData = Ys(quadInds(i,:));
+    h.ZData = Zs(quadInds(i,:));
+    h2 = f.Children(length(f.Children)).Children(2*i);
+    h2.XData = Xs(quadInds(i,:));% for tube
+    h2.YData = Ys(quadInds(i,:));% for tube
+    h2.ZData = -Zs(quadInds(i,:));% for tube
+    if i>4  
+        h3 = f.Children(length(f.Children)).Children(2*size(quadInds,1)+i-4); % for Miura flip
+        h3.XData = Xs(quadInds(i,:));% for tube
+        h3.YData = Ys(quadInds(i,:));% for tube
+        if i == 5 % hacky miura flip thing
+            h3.ZData = [Zs(quadInds(i,1:2)); 2*Zs(quadInds(i,2))-Zs(quadInds(i,3)); -Zs(quadInds(i,4))];
+        elseif i == 6
+            h3.ZData = [-Zs(quadInds(i,1)); 2*Zs(quadInds(i,3))-Zs(quadInds(i,2)); Zs(quadInds(i,3:4))];
+        elseif i == 7
+            h3.ZData = [Zs(quadInds(i,1:2)); -Zs(quadInds(i,3)); 2*Zs(quadInds(i,1))-Zs(quadInds(i,4)); ];
+        elseif i == 8
+            h3.ZData = [2*Zs(quadInds(i,4))-Zs(quadInds(i,1));-Zs(quadInds(i,2));  Zs(quadInds(i,3:4))];
+        end
+    end
+    %     s1 = [Xs(quadInds(i,2))-Xs(quadInds(i,1)); Ys(quadInds(i,2))-Ys(quadInds(i,1)); Zs(quadInds(i,2))-Zs(quadInds(i,1)) ];
+    %     s2 = [Xs(quadInds(i,4))-Xs(quadInds(i,1)); Ys(quadInds(i,4))-Ys(quadInds(i,1)); Zs(quadInds(i,4))-Zs(quadInds(i,1)) ];
+    %     n = cross(s1,s2);
+    %     nhats(:,i) = n/norm(n);
+end
+% % for checking angles for thick panels
+% rhom1 = pi-acos(nhats(:,2)'*nhats(:,4));       % angle of M and V of Miura (creases in YZ plane)
+% if (abs(pi-acos(nhats(:,1)'*nhats(:,3)))-rhom1 > deg2rad(0.1))
+%     error('Do not match');
+% end
+% rhom2 = pi-acos(nhats(:,1)'*nhats(:,2));       % angle of the Ms of Miura (creases parallel to XY plane)
+% rhom2sup = pi-acos(nhats(:,2)'*[nhats(1:2,2); -nhats(3,2)]);      % angle between Miuras for tube
+% rhoe1 = pi-acos(nhats(:,6)'*nhats(:,8));       % angle of creases in YZ plane
+% rhoe2 = pi-acos(nhats(:,5)'*nhats(:,6));       % angle of creases in XZ plane
+%
+% tau1 = pi-acos(nhats(:,4)'*nhats(:,6));       % angle between Miura and Eggbox at +Y side
+% tau2 = pi-acos(nhats(:,3)'*nhats(:,5));       % angle between Miura and Eggbox at -Y side
 
 psims = linspace(0,gm,nplot);
 Sms = bm*sqrt(sin(gm).^2-sin(psims).^2)./cos(psims);
